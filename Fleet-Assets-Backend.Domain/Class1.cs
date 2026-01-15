a9f7e0dc47d9621c81d7d204f5a56a842abede65
Tiberiu Hurjui <atiberiu88@gmail.com>
2026-01-15 09:20:23 +0200

    project-arhitecture

---
+++ Fleet-Assets-Backend.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Fleet_Assets_Backend.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Fleet-Assets-Backend.Infrasturcture/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Fleet_Assets_Backend.Infrasturcture
+{
+    public class Class1
+    {
+
+    }
+}
